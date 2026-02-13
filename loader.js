0ed762f655a6856da08752850ea65c7bf0114f63
ajoysayeed <ajoysayeed@gmail.com>
2026-02-13 18:30:49 +0300

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // ЗАМЕНИ ЭТУ ССЫЛКУ на свою прямую ссылку из GitHub Pages (см. инструкцию ниже)
+    const url = 'https://mysite.com/table.html';
+
+    const container = document.getElementById('affiliate-table-container');
+    if (container) {
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                container.innerHTML = data;
+            })
+            .catch(error => console.error('Error loading table:', error));
+    }
+});
